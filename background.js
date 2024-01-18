20ef6e05305bdd0e1503317823a2746aae4deed2
Bogdan Sochivko <30751786+BogdanSochivko@users.noreply.github.com>
2024-01-18 10:18:40 +0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    if (details.url.startsWith("https://stats.vk-portal.net/web-stats/p")) {
+      details.requestHeaders.push({ name: "Origin", value: "0.626.18.3_0" });
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  { urls: ["https://stats.vk-portal.net/*"] },
+  ["blocking", "requestHeaders"]
+);
