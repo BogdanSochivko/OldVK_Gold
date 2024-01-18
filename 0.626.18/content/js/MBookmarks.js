79b9422448d660ffc446f75995b459d0676629e0
Bogdan Sochivko <30751786+BogdanSochivko@users.noreply.github.com>
2024-01-18 10:15:47 +0600

    Delete 0.626.18 directory

--- 0.626.18/content/js/MBookmarks.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  "use strict";
-  new MutationObserver(function() {
-    if(document.title == "Закладки") document.title = "Мои закладки";
-  }).observe(document.head, {childList:true, subtree: true});
-})();
--- 0.626.18/content/js/MVid.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  "use strict";
-  new MutationObserver(function() {
-    if(document.title == "Мои видео") document.title = "Мои видеозаписи";
-  }).observe(document.head, {childList:true, subtree: true});
-})();
