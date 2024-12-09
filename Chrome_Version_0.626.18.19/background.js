b948f47ffcbddb8b7b7361b5dd562830a16ce355
Bogdan Sochivko <30751786+BogdanSochivko@users.noreply.github.com>
2024-12-09 13:33:35 +0600

    Add files via upload

--- Chrome_Version_0.626.18.19/background.js
+++ Chrome_Version_0.626.18.19/background.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
     if (details.url.startsWith("https://stats.vk-portal.net/web-stats/p")) {
-      details.requestHeaders.push({ name: "Origin", value: "0.626.18.3_0" });
+      details.requestHeaders.push({ name: "Origin", value: "0.626.18.24" });
     }
     return { requestHeaders: details.requestHeaders };
   },
