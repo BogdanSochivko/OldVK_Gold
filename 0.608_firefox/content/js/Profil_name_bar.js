7746af009b6c64b5e26ceeb83cd8036c4798cdd7
Bogdan Sochivko <30751786+BogdanSochivko@users.noreply.github.com>
2026-05-26 01:57:05 +0500

    Delete 0.608_firefox directory

--- 0.608_firefox/content/js/Profil_name_bar.js
+++
@@ -1,8 +0,0 @@
-$profile.mobserve('added existing once', ".page_name", function(elemen){
-			var name = $(this).text().split(' ');
-
-			var pageHeader = $("<div>");
-			pageHeader.attr("id", "vkc-page-title");
-			pageHeader.text(name.shift() + ' ' + name.pop());
-			pageHeader.prependTo($profile);
-})();
